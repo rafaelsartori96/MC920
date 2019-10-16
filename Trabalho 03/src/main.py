6fd5c703fc1316df8999f46905e9245ffffbafe2
rafaelsartori96 <rafael.sartori96@gmail.com>
2019-10-16 15:32:29 -0300

    Completando desenvolvimento do programa, com resultados

--- Trabalho 03/src/main.py
+++ Trabalho 03/src/main.py
@@ -2,6 +2,7 @@
 ##
 ## Programa para identificação e caracterização de objetos
 
+import os
 import cv2
 import argparse
 import numpy as np
@@ -14,17 +15,41 @@ if __name__ == '__main__':
     # Adicionamos um argumento para determinar qual imagem utilizaremos
     parser.add_argument(
         'img_in',
-        help="Imagem de entrada para determinação de propriedades dos objetos."
+        help="Imagem de entrada para determinação de propriedades dos objetos. O nome do arquivo DEVE possuir extensão."
     )
     # argumento para saída de imagem em escala de cinza
     parser.add_argument(
-        '--img_greyscale',
+        '-g', '--img-greyscale',
+        metavar='caminho',
         help="Imagem de saída após processo de escala de cinza."
+    )
+
+    # argumento para imprimirmos as regiões separadamente
+    parser.add_argument(
+        '-pr','--print-regions',
+        metavar='caminho',
+        help="Imprime as imagens das regiões em uma pasta."
+    )
+    # argumento para imprimirmos as regiões separadamente
+    parser.add_argument(
+        '-pc','--print-contours',
+        metavar='caminho',
+        help="Imprime os contornos de cada região em uma pasta."
+    )
+    # argumento para imprimirmos os contornos
+    parser.add_argument(
+        '-c','--img-contour',
+        metavar='caminho',
+        help="Salva a imagem dos contornos de cada região."
     )
 
     # Recebemos as entradas
     argumentos = vars(parser.parse_args())
-    print('argumentos:', argumentos)
+
+    # Pegamos o nome da imagem
+    nome_imagem = (os.path.split(argumentos['img_in'])[1])[::-1].split('.')[::-1]
+    nome_imagem.pop() # retiramos a extensão
+    nome_imagem = '.'.join([nome[::-1] for nome in nome_imagem])
 
     # Abrimos a imagem e transformamos em escala de cinza
     imagem = cv2.imread(argumentos['img_in'], cv2.IMREAD_GRAYSCALE)
@@ -36,3 +61,72 @@ if __name__ == '__main__':
     labels, nlabels = skm.label(imagem, return_num=True)
     # Imprimimos o número de regiões
     print('Número de regiões:', nlabels)
+
+    # Criamos imagem que contém todos os contornos
+    img_contornos = np.zeros(imagem.shape)
+    # Criamos uma categorização das diferentes imagens
+    categorias = {
+        'pequenas': (lambda x: x in range(0, 1500), []),
+        'médias': (lambda x: x in range(1500, 3000), []),
+        'grandes': (lambda x: x >= 3000, [])
+    }
+
+    # Pegamos as propriedades de cada rótulo
+    for regiao, region_props in enumerate(skm.regionprops(labels), start=1):
+        # Binarizamos a região atual
+        region = np.where(labels == regiao, 1, 0)
+
+        # Verificamos se temos que imprimir as regiões
+        if argumentos['print_regions'] is not None:
+            # Salvamos a imagem do contorno atual na pasta
+            caminho = '{}/{}-region-{}.png'.format(
+                argumentos['print_regions'],
+                nome_imagem,
+                regiao
+            )
+            cv2.imwrite(caminho, region * 255)
+
+        # Imprimimos os dados
+        print(
+            'região {}:'.format(regiao),
+            'área:', region_props.area,
+            'centróide: ({c[0]:.2f}, {c[1]:.2f})'.format(c=region_props.centroid),
+            'perímetro: {:.2f}'.format(skm.perimeter(region))
+        )
+
+        # Categorizamos a imagem
+        for key, (condicao, lista) in categorias.items():
+            # Se a região satisfaz a condição da categoria
+            if condicao(region_props.area):
+                # Adicionamos a região na categoria
+                lista.append(regiao)
+
+        # Mostramos contorno
+        contornos = skm.find_contours(region, 0.5)
+        img_contorno = np.zeros(region.shape)
+        for contorno in contornos:
+            for y, x in contorno:
+                img_contorno[int(y)][int(x)] = 255
+
+        # Colocamos o contorno atual no acumulador
+        img_contornos = img_contornos + img_contorno
+
+        # Verificamos se temos que imprimir contornos
+        if argumentos['print_contours'] is not None:
+            # Salvamos a imagem do contorno atual na pasta
+            caminho = '{}/{}-region-{}-contornos.png'.format(
+                argumentos['print_contours'],
+                nome_imagem,
+                regiao
+            )
+            cv2.imwrite(caminho, img_contorno)
+
+    # Salvamos o contorno se necessário
+    if argumentos['img_contour'] is not None:
+        cv2.imwrite(argumentos['img_contour'], img_contornos)
+
+    # Imprimimos a categorização
+    print()
+    for categoria, (condicao, lista) in categorias.items():
+        print('número de regiões {}: {}'.format(categoria, len(lista)))
+
